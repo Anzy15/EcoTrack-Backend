274570726e043c343720e59a5d500f742dd0ed57
Anzyyy <foxtrot091503@gmail.com>
2025-02-17 13:57:35 +0800

    Springboot Set-Up

---
+++ EcoTrack/EcoTrack/src/test/java/com/capstone/EcoTrack/EcoTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.EcoTrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoTrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
